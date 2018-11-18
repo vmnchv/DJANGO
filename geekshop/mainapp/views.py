0d252a013ebc31996adee1bf8328ed589da6ad07
vmnchv <vmnchv@gmail.com>
2018-11-18 13:44:21 +0300

    django

---
+++ geekshop/mainapp/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+def main (request):
+    return render(request, 'mainapp/index.html')
+def products (request):
+    return render(request, 'mainapp/products.html')
+def contact (request):
+    return render(request, 'mainapp/contact.html')
+
+
+# Create your views here.
